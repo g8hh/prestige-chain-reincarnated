1b3b93c5c8f765de437c3e8d30b293add0a07186
pg132 <picogilman@gmail.com>
2022-01-11 18:21:34 -0800

    C: v.001.3

--- js/layers.js
+++ js/layers.js
@@ -88,6 +88,232 @@ function filterOut(list, out){
         return list.filter(x => !out.includes(x) && !out.includes(Number(x)))
 }
 
+addLayer("a", {
+        name: "Alligators", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
+        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+        startData() { return {
+                unlocked: false,
+		points: new Decimal(0),
+                best: new Decimal(0),
+                total: new Decimal(0),
+                abtime: 0,
+                time: 0,
+                times: 0,
+                autotimes: 0,
+        }},
+        color: "#BB4C83",
+        branches: [],
+        requires: new Decimal(0), // Can be a function that takes requirement increases into account
+        resource: "Alligators", // Name of prestige currency
+        baseResource: "points", // Name of resource prestige is based on
+        baseAmount() {return player.points.floor()}, // Get the current amount of baseResource
+        type: "custom", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+        getResetGain() {
+                return getGeneralizedPrestigeGain("a")
+        },
+        getBaseDiv(){
+                let x = new Decimal(1)
+                return x
+        },
+        getGainExp(){
+                let x = new Decimal(2)
+                if (hasUpgrade("a", 32)) x = x.times(3)
+
+                x = x.plus(tmp.a.buyables[21].effect)
+                x = x.plus(getGoalChallengeReward("00"))
+
+                return x
+        },
+        getGainMultPre(){
+                let x = new Decimal(1)
+                return x
+        },
+        getGainMultPost(){
+                let x = getGeneralizedInitialPostMult("a")
+
+                if (hasUpgrade("a", 13)) x = x.times(upgradeEffect("a", 13))
+                if (hasUpgrade("a", 14)) x = x.times(upgradeEffect("a", 14))
+                if (hasUpgrade("a", 23)) x = x.times(2)
+                                         x = x.times(getBuyableEffect("a", 12))
+                if (hasUpgrade("b", 11)) x = x.times(upgradeEffect("b", 11))
+                                         x = x.times(getBuyableEffect("a", 31))
+                                         x = x.times(getBuyableEffect("b", 21))
+                                         x = x.times(getBuyableEffect("c", 23))
+                                         x = x.times(tmp.goalsii.effect)
+
+                return x
+        },
+        effect(){
+                if (!isPrestigeEffectActive("a")) return new Decimal(1)
+
+                let amt = player.a.points
+
+                let exp = new Decimal(.5)
+                exp = exp.plus(CURRENT_BUYABLE_EFFECTS["f32"])
+
+                let ret = amt.plus(1).pow(exp)
+
+                return ret
+        },
+        effectDescription(){
+                return getGeneralizedEffectDisplay("a")
+        },
+        update(diff){
+                let data = player.a
+
+                if (tmp.a.getResetGain.gt(0)) data.unlocked = true
+
+                data.best = data.best.max(data.points)
+                if (false) {
+                        data.points = data.points.plus(tmp.a.getResetGain.times(diff))
+                        data.total   = data.total.plus(tmp.a.getResetGain.times(diff))
+                        data.autotimes += diff
+                        if (data.autotimes > 10) data.autotimes = 10
+                        if (data.autotimes > 1) {
+                                data.autotimes += -1
+                                data.times ++
+                        }
+                }
+                if (false) {
+                        handleGeneralizedBuyableAutobuy(diff, "a")
+                } else {
+                        data.abtime = 0
+                }
+                data.time += diff
+        },
+        row: 0, // Row the layer is in on the tree (0 is the first row)
+        layerShown(){return true},
+        prestigeButtonText(){
+                if (false) return ""
+                return getGeneralizedPrestigeButtonText("a")
+        },
+        canReset(){
+                return player.a.time >= 2 && !false && tmp.a.getResetGain.gt(0)
+        },
+        upgrades: {
+                rows: 5,
+                cols: 5,
+                11: {
+                        title: "And",
+                        description: "Amoebas boost point gain",
+                        cost: new Decimal(2),
+                        effect(){
+                                if (inChallenge("b", 12)) return new Decimal(1)
+                                
+                                let exp = 3
+                                if (hasUpgrade("a", 21)) exp += player.a.upgrades.length * .5
+
+                                if (hasUpgrade("a", 44)) exp *= exp
+                                if (hasUpgrade("c", 11)) exp *= 2
+
+                                let ret = player.a.points.times(10).plus(20).log10().pow(exp)
+                                return ret
+                        },
+                        effectDisplay(){
+                                if (player.tab != "a") return ""
+                                if (player.subtabs.a.mainTabs != "Upgrades") return ""
+                                return format(tmp.a.upgrades[11].effect)
+                        },
+                        unlocked(){
+                                return player.a.best.gt(0) || hasUnlockedPast("a")
+                        }, //hasUpgrade("a", 11)
+                },
+        },
+        buyables: {
+                rows: 3,
+                cols: 3,
+                11: getGeneralizedBuyableData("a", 11, function(){
+                        return hasUpgrade("a", 15) //|| player.b.unlocked
+                        }),
+                12: getGeneralizedBuyableData("a", 12, function(){
+                        return hasUpgrade("a", 22) //|| player.b.unlocked
+                        }),
+                13: getGeneralizedBuyableData("a", 13, function(){
+                        return hasUpgrade("a", 31) //|| player.b.unlocked
+                        }),
+                21: getGeneralizedBuyableData("a", 21, function(){
+                        return hasUpgrade("b", 21) //|| player.b.unlocked
+                        }),
+                22: getGeneralizedBuyableData("a", 22, function(){
+                        return hasUpgrade("b", 24) //|| player.b.unlocked
+                        }),
+                23: getGeneralizedBuyableData("a", 23, function(){
+                        return hasUpgrade("c", 12) //|| player.b.unlocked
+                        }),
+                31: getGeneralizedBuyableData("a", 31, function(){
+                        return hasUpgrade("a", 53) //|| player.b.unlocked
+                        }),
+                32: getGeneralizedBuyableData("a", 32, function(){
+                        return hasUpgrade("b", 43) //|| player.b.unlocked
+                        }),
+                33: getGeneralizedBuyableData("a", 33, function(){
+                        return hasUpgrade("b", 51) //|| player.b.unlocked
+                        }),
+        },
+        tabFormat: {
+                "Upgrades": {
+                        content: ["main-display",
+                                ["prestige-button", "", function (){ return false ? {'display': 'none'} : {}}],
+                                ["display-text",
+                                        function() {
+                                                return shiftDown ? "Your best Alligators is " + format(player.a.best) : "You have done " + formatWhole(player.a.times) + " Alligator resets"
+                                        }
+                                ],
+                                ["display-text",
+                                        function() {
+                                                if (false) return "You are gaining " + format(tmp.a.getResetGain) + " Alligators per second"
+                                                return "There is a two second cooldown for prestiging (" + format(Math.max(0, 2-player.a.time)) + ")" 
+                                        },
+                                ],
+                                "blank", 
+                                "upgrades"],
+                        unlocked(){
+                                return true
+                        },
+                },
+                "Buyables": {
+                        content: ["main-display",
+                                ["display-text",
+                                        function() {
+                                                if (false) return "You are gaining " + format(tmp.a.getResetGain) + " Alligators per second"
+                                                return ""
+                                        },
+                                ],
+                                "buyables"],
+                        unlocked(){
+                                return false //|| player.b.unlocked
+                        },
+                },
+        },
+        doReset(layer){
+                let data = player.a
+                if (layer == "a") data.time = 0
+                if (!getsReset("a", layer)) return
+                
+                data.times = 0
+
+                if (!false) { //upgrades
+                        let keptUpgrades = 0
+                        if (false) keptUpgrades += 0
+                        if (!false) {
+                                data.upgrades = data.upgrades.slice(0, keptUpgrades)
+                        }
+                }
+
+                //resources
+                data.points = new Decimal(0)
+                data.total = new Decimal(0)
+                data.best = new Decimal(0)
+
+                //buyables
+                let resetBuyables = [11, 12, 13, 21, 22, 23, 31, 32, 33]
+                for (let j = 0; j < resetBuyables.length; j++) {
+                        player.a.buyables[resetBuyables[j]] = new Decimal(0)
+                }
+        },
+})
+
 addLayer("ach", {
         name: "Achievements",
         symbol: "⭑", 
